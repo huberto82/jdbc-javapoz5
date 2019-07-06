bdab4fbd5db6750b1e4ffe600ac9b2fd3388df3a
cs <siwon.cezary@gmail.com>
2019-07-06 11:36:09 +0200

    connection and table create

--- src/main/java/ConnectDemo.java
+++ src/main/java/ConnectDemo.java
@@ -3,11 +3,38 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import static java.sql.Connection.*;
+
 public class ConnectDemo {
 
     public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException, SQLException {
-        Class.forName("org.h2.Driver").newInstance();
-        Connection con = DriverManager.getConnection("jdbc:h2:mem:testdb", "sa","");
+        Connection con = getConnection();
+        Statement stat = prepareBase(con);
+        if (stat.execute("SELECT * FROM PERSON")){
+            System.out.println("SELECT SUCCESS");
+        }
+        con.setTransactionIsolation(TRANSACTION_SERIALIZABLE);
+        int isolation = con.getTransactionIsolation();
+        switch(isolation){
+            case TRANSACTION_NONE:
+                System.out.println("BRAK");
+                break;
+            case TRANSACTION_READ_UNCOMMITTED:
+                System.out.println("READ UNCOMMITTED");
+                break;
+            case TRANSACTION_READ_COMMITTED:
+                System.out.println("READ COMMITTED");
+                break;
+            case TRANSACTION_REPEATABLE_READ:
+                System.out.println("REPEATABLE");
+                break;
+            case TRANSACTION_SERIALIZABLE:
+                System.out.println("SERIALIZABLE");
+                break;
+        }
+    }
+
+    public static Statement prepareBase(Connection con) throws SQLException {
         Statement stat = con.createStatement();
         String create = "CREATE TABLE PERSON(" +
                 "id int primary key," +
@@ -15,5 +42,16 @@ public class ConnectDemo {
                 "last_name varchar(30) not null," +
                 "email varchar(20))";
         stat.executeUpdate(create);
+        int count = stat.executeUpdate(
+                "INSERT INTO PERSON VALUES (1, 'ADAM', 'NOWAK','adam@op/pl')");
+        if (count == 1){
+            System.out.println("ROW ADDED");
+        }
+        return stat;
+    }
+
+    public static Connection getConnection() throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+        Class.forName("org.h2.Driver").newInstance();
+        return DriverManager.getConnection("jdbc:h2:mem:testdb", "sa","");
     }
 }
---
+++ src/main/java/StatementDemo.java
@@ -0,0 +1,2 @@
+public class StatementDemo {
+}
